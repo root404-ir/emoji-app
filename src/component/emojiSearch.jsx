0e85f8f642dc746b91ad75dda842cb93c376ecda
root404-ir <mohammadrg360@gmail.com>
2024-11-12 13:47:53 +0330

    handle copy emoji

--- src/component/emojiSearch.jsx
+++ src/component/emojiSearch.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react"
 import EmojiList from '../EMOJI-ITEM.json'
 import '../assets/style/emojiSearch.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCopy } from '@fortawesome/free-solid-svg-icons';
+
 const Emoji = () => {
     const [searchItem, setSearchItem] = useState('')
 
@@ -8,18 +11,36 @@ const Emoji = () => {
         setSearchItem(e.target.value)
     }
     const filterEmoji = EmojiList.filter(emoji => (
-        emoji.title.toLowerCase().includes(searchItem.toLocaleLowerCase())
+        emoji.title.toLowerCase().includes(searchItem.toLowerCase())
     ))
+    const Copy = (emojiSymbol) => {
+        navigator.clipboard.writeText(emojiSymbol).then(() => {
+            alert('ایموجی مورد نظر با موفقیت کپی شد')
+        }).catch(err => {
+            console.error("خطا در کپی کردن ایموجی:", err)
+        })
+
+    }
     return (
         <div>
-            <h2>جستجوی ایموجی</h2>
-            <input type="text" placeholder="جستجوی ایموجی" value={searchItem} onChange={handleSearch} />
+            <div className="input">
+                <h2>جستجوی ایموجی</h2>
+                <input type="text" placeholder="جستجوی ایموجی" value={searchItem} onChange={handleSearch} />
+            </div>
             <ul>
                 {filterEmoji.map(e => (
-                    <li>
-                        {e.symbol}
-                        {e.title}
-                    </li>
+                    <div >
+                        <li className="emoji-container">
+                            {e.symbol} {e.title}
+                            <div className="icon">
+                                <FontAwesomeIcon icon={faCopy} onClick={() => Copy(e.symbol)} />
+                                <div className="copy">
+                                    <span>کپی</span>
+                                </div>
+                            </div>
+                        </li>
+                    </div>
+
                 ))}
             </ul>
         </div>
